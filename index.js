061997cbe3d95d9af3204217aee54ac59acb49ec
samuel-gonzalez2 <samuel.gonzalez@enroutesystems.com>
2022-05-18 17:57:36 -0500

    [CREATED] routes with HTTP methods

--- index.js
+++ index.js
@@ -9,12 +9,26 @@ app.use(parser.json())
 
 app.get('/', async (req, res) => {
     const {data} = await axios.get('https://jsonplaceholder.typicode.com/users')
-    res.send(data)
+    res.send(data).status(200)
 })
 
 app.post("/", async (req, res) => {
-    const {data} = await axios.post('https://jsonplaceholder.typicode.com/users')
-    res.send(data)
+    const {body} = req
+    const {data} = await axios.post('https://jsonplaceholder.typicode.com/users', body)
+    res.send(data).status(201)
+})
+
+app.put("/:id", async (req, res) => {
+    const {body}= req
+    const {id} = req.params
+    await axios.put( `https://jsonplaceholder.typicode.com/users/${id}`, body)
+    res.sendStatus(204)
+})
+
+app.delete("/:id", async (req, res) => {
+    const {id} = req.params
+    await axios.delete( `https://jsonplaceholder.typicode.com/users${id}`)
+    res.sendStatus(204)
 })
 
 app.listen(port, () => {
